532d235e51cebbd31d94e93720220f55478ea128
rmawb <robertmauban@gmail.com>
2020-06-21 19:43:10 -0700

    Started core on minecraft

---
+++ minecraft/core.py
@@ -0,0 +1,11 @@
+def get(server_name, path):
+    print("Installing minecraft")
+
+def start(server_name, path):
+    print("Starting", server_name)
+
+def stop(server_name, path):
+    print("Stopping", server_name)
+
+def restart(server_name, path):
+    print("Restarting", server_name)
